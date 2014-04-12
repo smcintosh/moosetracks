f9691a25145c37249642c18acfad0596b6c246d9
Shane McIntosh <shanemcintosh@acm.org>
2014-04-11 23:38:48 -0700

    Clean up old cruft in the build scripts

--- make/thirdparty.mk
+++ make/thirdparty.mk
@@ -1,4 +1,4 @@
 # SQLite library: tested against version 3.7.13.
-SQLITE_PATH = /opt/sqlite/3.7.13
-INC_SQLITE = $(SQLITE_PATH)/h
+SQLITE_PATH = /usr
+INC_SQLITE = $(SQLITE_PATH)/include
 LIB_SQLITE = $(SQLITE_PATH)/lib/libsqlite3$D
